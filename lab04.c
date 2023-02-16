b160ffc6cb9630789a99be7ec62043cad0342657
shanroxalot <47994861+shanroxalot@users.noreply.github.com>
2023-02-16 00:41:42 +0000

    error check for same file in arguments

--- lab04.c
+++ lab04.c
@@ -17,7 +17,12 @@ int main(int argc, char *argv[]) {
 		printf("Usage: %s <source_filename> <destination_filename>\n", argv[0]);
 		exit (-1);
 	}
-	
+
+	if (*argv[1] == *argv[2]){
+		printf("Error: Source file and destination file are the same\n");
+		exit (-1);
+	}
+
 	readFileDescriptor = open(argv[2], O_RDONLY);
 	writeFileDescriptor = open(argv[1], O_WRONLY, 0700);
 	
@@ -25,6 +30,7 @@ int main(int argc, char *argv[]) {
 		printf("Error with file open\n");
 		exit (-1);
 	}
+
 
     if (lseek(writeFileDescriptor, 0, SEEK_END) >=0);{
         write(writeFileDescriptor, "\n\n", 2);
